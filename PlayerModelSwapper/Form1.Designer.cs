6b9246a0932d7e8ffed8c6692bf10b549b63f8a9
Szikaka <michalmuszka@interia.pl>
2021-12-31 17:17:50 +0100

    Dodaj pliki projektów.

---
+++ PlayerModelSwapper/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace PlayerModelSwapper {
+    partial class PlayerModelSwapper {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayerModelSwapper));
+            this.textureDialog = new System.Windows.Forms.OpenFileDialog();
+            this.directoryDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.textureButton = new System.Windows.Forms.Button();
+            this.updateLabel = new System.Windows.Forms.Label();
+            this.directoryButton = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.workButton = new System.Windows.Forms.Button();
+            this.TextureIndicator = new System.Windows.Forms.PictureBox();
+            this.directoryIndicator = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.finishedIndicator = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.TextureIndicator)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.directoryIndicator)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.finishedIndicator)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textureDialog
+            // 
+            this.textureDialog.FileName = "openFileDialog1";
+            // 
+            // textureButton
+            // 
+            this.textureButton.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textureButton.Location = new System.Drawing.Point(12, 319);
+            this.textureButton.Name = "textureButton";
+            this.textureButton.Size = new System.Drawing.Size(150, 40);
+            this.textureButton.TabIndex = 0;
+            this.textureButton.Text = "Select Texture File";
+            this.textureButton.UseVisualStyleBackColor = true;
+            this.textureButton.Click += new System.EventHandler(this.textureButton_Click);
+            // 
+            // updateLabel
+            // 
+            this.updateLabel.AutoSize = true;
+            this.updateLabel.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.updateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.updateLabel.Location = new System.Drawing.Point(263, 13);
+            this.updateLabel.MinimumSize = new System.Drawing.Size(120, 230);
+            this.updateLabel.Name = "updateLabel";
+            this.updateLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.updateLabel.Size = new System.Drawing.Size(120, 230);
+            this.updateLabel.TabIndex = 2;
+            this.updateLabel.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // directoryButton
+            // 
+            this.directoryButton.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.directoryButton.Location = new System.Drawing.Point(200, 319);
+            this.directoryButton.Name = "directoryButton";
+            this.directoryButton.Size = new System.Drawing.Size(180, 40);
+            this.directoryButton.TabIndex = 5;
+            this.directoryButton.Text = "Select Game Directory";
+            this.directoryButton.UseVisualStyleBackColor = true;
+            this.directoryButton.Click += new System.EventHandler(this.directoryButton_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "disabled.png");
+            this.imageList1.Images.SetKeyName(1, "enabled.png");
+            // 
+            // workButton
+            // 
+            this.workButton.Enabled = false;
+            this.workButton.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.workButton.Location = new System.Drawing.Point(420, 319);
+            this.workButton.Name = "workButton";
+            this.workButton.Size = new System.Drawing.Size(150, 40);
+            this.workButton.TabIndex = 6;
+            this.workButton.Text = "Mod";
+            this.workButton.UseVisualStyleBackColor = true;
+            this.workButton.Click += new System.EventHandler(this.workButton_Click);
+            // 
+            // TextureIndicator
+            // 
+            this.TextureIndicator.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.TextureIndicator.Image = global::PlayerModelSwapper.Properties.Resources.disabled;
+            this.TextureIndicator.Location = new System.Drawing.Point(0, 0);
+            this.TextureIndicator.Name = "TextureIndicator";
+            this.TextureIndicator.Size = new System.Drawing.Size(50, 50);
+            this.TextureIndicator.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.TextureIndicator.TabIndex = 7;
+            this.TextureIndicator.TabStop = false;
+            // 
+            // directoryIndicator
+            // 
+            this.directoryIndicator.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.directoryIndicator.Image = global::PlayerModelSwapper.Properties.Resources.disabled;
+            this.directoryIndicator.Location = new System.Drawing.Point(65, 0);
+            this.directoryIndicator.Name = "directoryIndicator";
+            this.directoryIndicator.Size = new System.Drawing.Size(50, 50);
+            this.directoryIndicator.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.directoryIndicator.TabIndex = 4;
+            this.directoryIndicator.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(471, 13);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(102, 78);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Modding Tool \r\nby Szikaka\r\nver. 1.0.0\r\n\r\nFor support, contact\r\nSzikakA#3853";
+            // 
+            // finishedIndicator
+            // 
+            this.finishedIndicator.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.finishedIndicator.Image = global::PlayerModelSwapper.Properties.Resources.disabled;
+            this.finishedIndicator.Location = new System.Drawing.Point(130, 0);
+            this.finishedIndicator.Name = "finishedIndicator";
+            this.finishedIndicator.Size = new System.Drawing.Size(50, 50);
+            this.finishedIndicator.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.finishedIndicator.TabIndex = 9;
+            this.finishedIndicator.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.TextureIndicator);
+            this.panel1.Controls.Add(this.finishedIndicator);
+            this.panel1.Controls.Add(this.directoryIndicator);
+            this.panel1.Location = new System.Drawing.Point(200, 263);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 50);
+            this.panel1.TabIndex = 10;
+            // 
+            // PlayerModelSwapper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(584, 381);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.workButton);
+            this.Controls.Add(this.directoryButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.updateLabel);
+            this.Controls.Add(this.textureButton);
+            this.DoubleBuffered = true;
+            this.Name = "PlayerModelSwapper";
+            this.Text = "PlayerModelSwapper";
+            ((System.ComponentModel.ISupportInitialize)(this.TextureIndicator)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.directoryIndicator)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.finishedIndicator)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog textureDialog;
+        private System.Windows.Forms.FolderBrowserDialog directoryDialog;
+        private System.Windows.Forms.Button textureButton;
+        private System.Windows.Forms.Label updateLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox directoryIndicator;
+        private System.Windows.Forms.Button directoryButton;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button workButton;
+        private System.Windows.Forms.PictureBox TextureIndicator;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox finishedIndicator;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ PlayerModelSwapper/Form1.cs
@@ -0,0 +1,258 @@
+﻿// Tool by Szikaka //
+//   31.12.2021    //
+/////////////////////
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Diagnostics;
+
+namespace PlayerModelSwapper {
+    public partial class PlayerModelSwapper : Form {
+
+        private byte[] targetName = Encoding.ASCII.GetBytes("target_silhouette");
+        private byte[] endName = Encoding.ASCII.GetBytes("r2_exterior_concrete_grey_normal");
+
+        private string imagepath = "";
+        private string assetPath = "";
+        private bool textureProgress = false;
+        private bool assetProgress = false;
+        private bool finished = false;
+
+        private List<byte> textureFile;
+        private List<byte> assetFile;
+
+        public PlayerModelSwapper() {
+            InitializeComponent();
+        }
+
+        private async void textureButton_Click(object sender, EventArgs e) {
+            textureDialog.InitialDirectory = "../textures";
+            textureDialog.Filter = "Image Files | *.BMP; *.GIF *.JPG; *.PNG; *.TIFF | All files(*.*) | *.*";
+
+            if (textureDialog.ShowDialog() != DialogResult.OK) return;
+
+            Image image = null;
+
+            try {
+                image = new Bitmap(textureDialog.FileName);
+            } catch (ArgumentException exception) {
+                updateLabel.Text += "Invalid image file\n";
+                Console.WriteLine(exception.StackTrace);
+                return;
+            } 
+
+            if (image.Width != 1024 || image.Height != 2048) {
+                updateLabel.Text += "ERROR: Wrong bitmap dimensions\n";
+                return;
+            }
+
+            pictureBox1.Image = image;
+            
+            Process crunch = new Process();
+            crunch.StartInfo.WorkingDirectory = "Crunch/bin";
+            crunch.StartInfo.FileName = "crunch.exe";
+            crunch.StartInfo.Arguments = "\"" + textureDialog.FileName + "\" -DXT5 -yflip -fileformat dds -out out\\plik.dds";
+            crunch.Start();
+
+            crunch.WaitForExit();
+
+            imagepath = Directory.GetCurrentDirectory() + "\\Crunch\\bin\\out\\plik.dds";
+
+            if (File.Exists(imagepath)) Console.WriteLine("OK");
+            else {
+                updateLabel.Text += "Texture file error\n";
+                return;
+            }
+
+            Stream fileStream = File.Open(imagepath, FileMode.Open);
+            updateLabel.Text += "Preparing file...\n";
+            await Task.Run(() => {
+                MemoryStream ms = new MemoryStream();
+                fileStream.CopyTo(ms);
+
+                textureFile = new List<byte>(ms.ToArray());
+            });
+            updateLabel.Text += "Texture file prepared\n";
+
+            fileStream.Close();
+
+            File.Delete(imagepath);
+
+            textureProgress = true;
+
+            if (textureProgress && assetProgress) {
+                workButton.Enabled = true;
+            }
+
+            TextureIndicator.Image = imageList1.Images[1];
+            textureButton.Enabled = false;
+
+            fileStream.Close();
+        }
+
+        private async void directoryButton_Click(object sender, EventArgs e) {
+            if (directoryDialog.ShowDialog() != DialogResult.OK) return;
+
+            assetPath = directoryDialog.SelectedPath + "/Receiver2_Data/sharedassets2.assets";
+
+            if (!File.Exists(assetPath)) {
+                updateLabel.Text += "ERROR: Wrong game directory\n";
+                return;
+            }
+
+            if (!File.Exists(assetPath + ".old")) {
+                updateLabel.Text += "Creating backup file...\n";
+                File.Copy(assetPath, assetPath + ".old");
+                updateLabel.Text += "Backup file created\n";
+            }
+
+            Stream fileStream = File.Open(assetPath, FileMode.Open);
+
+            updateLabel.Text += "Preparing file...\n";
+
+            await Task.Run(() => {
+                MemoryStream ms = new MemoryStream();
+                fileStream.CopyTo(ms);
+
+                assetFile = new List<byte>(ms.ToArray());
+            });
+
+            updateLabel.Text += "File prepared\n";
+
+            assetProgress = true;
+
+            if (textureProgress && assetProgress) {
+                workButton.Enabled = true;
+            }
+
+            fileStream.Close();
+
+            directoryIndicator.Image = imageList1.Images[1];
+
+            directoryButton.Enabled = false;
+        }
+
+        private async void workButton_Click(object sender, EventArgs e) {
+
+            if (finished) this.Close();
+
+            updateLabel.Text += "Preparing to mod...\n";
+            updateLabel.Text += "Preparing texture file\n";
+
+            var textureData = textureFile.GetRange(128, textureFile.Count - 128);
+
+            updateLabel.Text += "Texture file prepared\n";
+
+            int startIndex = 0;
+            int endIndex = 0;
+
+            updateLabel.Text += "Preparing modded file\n";
+
+            Predicate<int> targetFind = (int index) => {
+                for (int i = 0; i < targetName.Length; i++) {
+                    if (assetFile[index + i] != targetName[i]) return false;
+                }
+                return true;
+            };
+
+            await Task.Run(() => {
+                for (; startIndex < assetFile.Count - 17; startIndex++) { 
+                    if (targetFind(startIndex)) break;
+                }
+            });
+
+            Predicate<int> endFind = (int index) => {
+                for (int i = 0; i < endName.Length; i++) {
+                    if (assetFile[index + i] != endName[i]) return false;
+                }
+                return true;
+            };
+
+            await Task.Run(() => {
+                for (endIndex = startIndex; endIndex <= startIndex + 3000000; endIndex++) {
+                    if (endFind(endIndex)) break;
+                }
+            });
+
+            if (endIndex == startIndex + 3000000) {
+                updateLabel.Text += "Asset file is invalid\n";
+                Console.WriteLine("FAIL");
+                return;
+            }
+
+            List<byte> moddedFileData = new List<byte>(assetFile.Take(startIndex + 96));
+
+            moddedFileData.AddRange(new byte[] {208, 170, 42, 0});
+
+            moddedFileData.AddRange(textureData);
+
+            moddedFileData.AddRange(new byte[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+
+            List<byte> moddedFileend = new List<byte>(assetFile.Skip(endIndex - 4));
+
+            moddedFileData.AddRange(moddedFileend);
+
+            moddedFileData[4] = (byte) (moddedFileData.Count >> 24);
+            moddedFileData[5] = (byte) (moddedFileData.Count >> 16);
+            moddedFileData[6] = (byte) (moddedFileData.Count >> 8);
+            moddedFileData[7] = (byte) (moddedFileData.Count);
+
+            int assetsCount = 0;
+
+            assetsCount = (assetsCount | moddedFileData[1432]);
+            assetsCount = (assetsCount | ((int) moddedFileData[1433] << 8));
+            assetsCount = (assetsCount | ((int) moddedFileData[1434] << 16));
+            assetsCount = (assetsCount | ((int) moddedFileData[1435] << 24));
+
+            int difference = moddedFileData.Count - assetFile.Count;
+            
+            for (int assetNum = 0; assetNum < assetsCount; assetNum += 1) {
+
+                if (assetNum == 16) {
+                    moddedFileData[1436 + (assetNum * 20) + 12] = (byte) (textureData.Count + 116);
+                    moddedFileData[1437 + (assetNum * 20) + 12] = (byte) ((textureData.Count + 116) >> 8);
+                    moddedFileData[1438 + (assetNum * 20) + 12] = (byte)((textureData.Count + 116) >> 16);
+                    moddedFileData[1439 + (assetNum * 20) + 12] = (byte)((textureData.Count + 116) >> 24);
+                }
+
+                if (assetNum > 16) {
+                    int assetOffset = 0;
+
+                    assetOffset = (assetOffset | moddedFileData[1436 + (assetNum * 20) + 8]);
+                    assetOffset = (assetOffset | ((int) moddedFileData[1437 + (assetNum * 20) + 8] << 8));
+                    assetOffset = (assetOffset | ((int) moddedFileData[1438 + (assetNum * 20) + 8] << 16));
+                    assetOffset = (assetOffset | ((int) moddedFileData[1439 + (assetNum * 20) + 8] << 24));
+
+                    assetOffset += difference;
+
+                    moddedFileData[1436 + (assetNum * 20) + 8] = (byte) (assetOffset & 0xFF);
+                    moddedFileData[1437 + (assetNum * 20) + 8] = (byte) ((assetOffset & 0xFF00) >> 8);
+                    moddedFileData[1438 + (assetNum * 20) + 8] = (byte) ((assetOffset & 0xFF0000) >> 16);
+                    moddedFileData[1439 + (assetNum * 20) + 8] = (byte) ((assetOffset & 0xFF000000) >> 24);
+                }
+            }
+
+            updateLabel.Text += "Modded file prepared\n";
+
+            Stream moddedAssetFile = File.Open(assetPath, FileMode.Create);
+            moddedAssetFile.Write(moddedFileData.ToArray(), 0, moddedFileData.Count);
+            moddedAssetFile.Close();
+
+            updateLabel.Text += "Modded file finished\n";
+
+            finished = true;
+            finishedIndicator.Image = imageList1.Images[1];
+            workButton.Text = "Close";
+        }
+    }
+}
